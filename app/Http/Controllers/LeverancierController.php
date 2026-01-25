b7a7552f5ef794318c445c0165bf0608bcd5dfb4
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-25 19:23:24 +0100

    feat: refactor LeverancierController methods to use integer ID and add error handling

--- app/Http/Controllers/LeverancierController.php
+++ app/Http/Controllers/LeverancierController.php
@@ -60,33 +60,73 @@ class LeverancierController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(LeverancierModel $leverancier)
+    public function show(int $leverancierId)
     {
+        $leverancier = $this->leverancierModel->getLeverancierWithContact($leverancierId);
 
-        $products = collect($this->leverancierModel->getProductsByLeverancierId($leverancier->Id))
+        if (!$leverancier) {
+            abort(404, 'Leverancier niet gevonden');
+        }
+
+        $products = collect($this->leverancierModel->getProductsByLeverancierId($leverancierId))
             ->sortByDesc('AantalInMagazijn')
             ->values();
 
         return view('leverancier.show', [
             'leverancier' => $leverancier,
             'products' => $products,
-            'title' => 'Geleverde producten',
+            'title' => 'Leverancier Details',
         ]);
     }
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(LeverancierModel $leverancierModel)
+    public function edit(int $leverancierId)
     {
-        //
+        $leverancier = $this->leverancierModel->getLeverancierWithContact($leverancierId);
+
+        if (!$leverancier) {
+            abort(404, 'Leverancier niet gevonden');
+        }
+
+        return view('leverancier.edit', [
+            'leverancier' => $leverancier,
+            'title' => 'Wijzig Leveranciergegevens',
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, LeverancierModel $leverancierModel)
+    public function update(Request $request, int $leverancierId)
     {
-        //
+        $leverancier = $this->leverancierModel->getLeverancierWithContact($leverancierId);
+
+        if (!$leverancier) {
+            abort(404, 'Leverancier niet gevonden');
+        }
+
+        $request->validate([
+            'naam' => 'required|string|max:60',
+            'contactpersoon' => 'required|string|max:60',
+            'leveranciernummer' => 'required|string|max:11',
+            'mobiel' => 'required|string|max:15',
+            'straat' => 'required|string|max:60',
+            'huisnummer' => 'required|integer|min:1',
+            'postcode' => 'required|string|max:10',
+            'stad' => 'required|string|max:60',
+        ]);
+
+        Log::warning('Leverancier update prevented due to simulated technical issue', [
+            'leverancierId' => $leverancierId,
+        ]);
+
+        return back()
+            ->withInput()
+            ->with([
+                'error' => 'Door een technische storing is het niet mogelijk de wijziging door te voeren. Probeer het op een later moment nog eens',
+                'redirect_to' => route('leverancier.show', $leverancierId),
+            ]);
     }
 
     /**
