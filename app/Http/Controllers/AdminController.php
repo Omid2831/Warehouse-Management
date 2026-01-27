9f5125c4480d8c4fbe5cbefb3397de009e6a3850
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-27 17:29:52 +0100

    feat: add unit tests for AdminController methods

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,15 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
+
+    private $userModel;
+
+    /**
+     * construct the userModel as the starter
+     */
+    public function __construct(User $userModel)
+    {
+        $this->userModel = $userModel;
+    }
     /**
      * Display the admin dashboard.
      */
     public function dashboard()
     {
         return view('administrator.dashboard');
+    }
+
+    /**
+     * manage user roles - Unit test
+     */
+    public function manageUserRoles()
+    {
+        $users = $this->userModel->getAllUsers(Auth::id());
+
+        return view('administrator.dashboard', [
+            'title' => 'Role management'
+            ,'users' => $users
+        ]);
+
     }
 }
