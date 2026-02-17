26651c46b2e52c6039de24f51b7f31980acb57f1
Omid2831 <mehrabiomid8282@gmail.com>
2026-02-17 18:45:50 +0100

    feat: add stored procedure for allergen selection with validation checks

---
+++ database/store_precedures/filtering/sp_allergeenSelection.sql
@@ -0,0 +1,46 @@
+USE aryanDBassignment;
+
+DROP PROCEDURE IF EXISTS sp_allergeenSelection;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_allergeenSelection(
+    IN p_allergeenNaam VARCHAR(30)
+)
+BEGIN
+
+    --  Check if NULL
+    IF p_allergeenNaam IS NULL OR p_allergeenNaam = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Please provide an allergen name.';
+    END IF;
+
+    --  Validate format
+    IF p_allergeenNaam NOT REGEXP '^[A-Za-z ]+$' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid input. Please enter a valid allergen name.';
+    END IF;
+
+    -- Check if allergen exists
+    IF NOT EXISTS (SELECT 1 FROM Allergeen WHERE Naam = p_allergeenNaam) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Allergen not found.';
+    END IF;
+
+    -- Return result
+    WITH allergen_data AS (
+        SELECT
+             ALGE.ID AS ID
+            ,ALGE.Naam AS Naam
+            ,ALGE.Omschrijving AS Omschrijving
+        FROM Allergeen AS ALGE
+        WHERE ALGE.Naam = p_allergeenNaam
+        ORDER BY Omschrijving ASC
+    )
+    SELECT
+       AD.Omschrijving AS Omschrijving
+    FROM allergen_data AS AD;
+
+END$$
+
+DELIMITER ;
