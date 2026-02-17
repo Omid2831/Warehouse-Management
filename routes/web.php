fbf11057df615e0711f561d36425ca43385c1b82
Omid2831 <mehrabiomid8282@gmail.com>
2026-02-17 19:56:17 +0100

    feat: add route for allergen selection information

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,7 @@ Route::middleware(['auth', 'role:admin'])
     */
         Route::prefix('allergeen')->as('allergeen.')->group(function () {
             Route::get('/', [AllergeenController::class, 'index'])->name('index');
+            Route::get('/selection-information', [AllergeenController::class, 'SelectionName'])->name('selection-information');
             Route::get('create', [AllergeenController::class, 'create'])->name('create');
             Route::post('/', [AllergeenController::class, 'store'])->name('store');
             Route::get('{id}/edit', [AllergeenController::class, 'edit'])->name('edit');
