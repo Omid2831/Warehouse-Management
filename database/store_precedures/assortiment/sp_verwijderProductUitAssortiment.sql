2ce632b3730f9faeee668774996329ffb187ae9e
Omid2831 <mehrabiomid8282@gmail.com>
2026-03-29 22:44:20 +0200

    feat: update sp_verwijderProductUitAssortiment to delete related records before product removal

--- database/store_precedures/assortiment/sp_verwijderProductUitAssortiment.sql
+++ database/store_precedures/assortiment/sp_verwijderProductUitAssortiment.sql
@@ -7,11 +7,13 @@ DROP PROCEDURE IF EXISTS sp_verwijderProductUitAssortiment;
 DELIMITER $$
 
 # Create the stored procedure
-CREATE PROCEDURE sp_verwijderProductUitAssortiment(IN p_productId INT)
+CREATE PROCEDURE sp_verwijderProductUitAssortiment(
+    IN p_productId INT
+)
 BEGIN
     DECLARE v_einddatum DATE;
     DECLARE v_today DATE;
-    
+
     -- Get the end delivery date for the product
     SELECT EindDatumLevering INTO v_einddatum
     FROM ProductEinddatumLevering
@@ -20,10 +22,13 @@ BEGIN
     SET v_today = CURDATE();
 
     IF v_today > v_einddatum THEN
-        -- You can either delete or set a flag, here we delete:
+        -- Delete from child tables first
+        DELETE FROM ProductPerAllergeen WHERE ProductId = p_productId;
+        DELETE FROM ProductPerLeverancier WHERE ProductId = p_productId;
         DELETE FROM ProductEinddatumLevering WHERE ProductId = p_productId;
-        -- Optionally, also delete from Product or set IsActief = 0
-        -- UPDATE Product SET IsActief = 0 WHERE Id = p_productId;
+        DELETE FROM Magazijn WHERE ProductId = p_productId;
+        -- Then delete from parent table
+        DELETE FROM Product WHERE Id = p_productId;
         SELECT 'Product succesvol verwijderd' AS message;
     ELSE
         SELECT 'Product kan niet worden verwijderd, datum van vandaag ligt voor einddatum levering' AS message;
